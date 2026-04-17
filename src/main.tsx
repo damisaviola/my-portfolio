e1a5c0da30b9c2ad9e73049a0387d2e9a64fe3c1
damisaviola <damimaturbongs@gmail.com>
2026-04-17 16:44:51 +0700

    feat: initialize React root and suppress console/error output in production mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Suppress console outputs and unhandled errors in production
+if (import.meta.env.PROD) {
+  console.log = () => {};
+  console.error = () => {};
+  console.warn = () => {};
+  console.debug = () => {};
+
+  window.onerror = function () {
+    return true; // Prevents the default browser error handler
+  };
+
+  window.onunhandledrejection = function (e) {
+    e.preventDefault(); // Prevents the default browser unhandled rejection handler
+  };
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
